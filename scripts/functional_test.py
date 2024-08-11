f68c69003f8a01fc92a076bea66c4f9028a02a0f
Rahulrockss <rahulskumar343@gmail.com>
2024-08-11 23:12:42 +0530

    Function Testing Update

--- scripts/functional_test.py
+++ scripts/functional_test.py
@@ -8,43 +8,46 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-# Set up Chrome WebDriver
+# Seting Chrome WebDriver
 chrome_options = webdriver.ChromeOptions()
-chrome_driver_path = ChromeDriverManager().install()  # Install and get path to ChromeDriver
+chrome_driver_path = ChromeDriverManager().install() 
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
 try:
     # Step 1: Navigate to the URL
     driver.get('https://demo.dealsdray.com/')
     
-    # Step 2: Log in with provided credentials
-    username = driver.find_element(By.CLASS_NAME, 'css-l8vkz1')  # Assuming ID 'email' for username field
-    password = driver.find_element(By.CLASS_NAME, 'css-r71t31')  # Assuming ID 'password' for password field
+    # Step 2: Log in
+    username = driver.find_element(By.CLASS_NAME, 'css-l8vkz1')  
+    password = driver.find_element(By.CLASS_NAME, 'css-r71t31') 
 
     username.send_keys('prexo.mis@dealsdray.com')
     password.send_keys('prexo.mis@dealsdray.com')
     
-    login_button = driver.find_element(By.XPATH, '//button[contains(text(),"Login")]')  # Adjust the XPATH according to the actual login button
+    login_button = driver.find_element(By.XPATH, '//button[contains(text(),"Login")]')  
     login_button.click()
 
-    # Step 3: Wait until logged in and dashboard is loadedcss-i9zxpg
+    # Step 3: Wait until logged
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(),"Dashboard")]')))
 
-    # Step 4: Navigate to the Orders section
+    # Step 4: Navigate to the Order section
     orders_section = driver.find_element(By.CLASS_NAME, 'css-1s178v5')
     orders_section.click()
     
+    # Step 5: Navigate to the Orders
     orders = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//span[contains(text(),"Orders")]')))
     orders.click()
-    # Step 5: Navigate to Add Bulk Order
+    
+    # Step 6: Navigate to Add Bulk Order
     add_bulk_order = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(),"Add Bulk Orders")]')))
-    # add_bulk_order.click()
+
     driver.execute_script("arguments[0].scrollIntoView();", add_bulk_order)
     driver.execute_script("arguments[0].click();", add_bulk_order)
-    # Step 6: Upload the XLS file
+    
+    # Step 7: Upload the XLS file
     upload_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//input[@type="file"]')))
     upload_button.send_keys('E:\Automation-Test-02---Functional-Testing-Case\scripts\demo-data.xlsx')  # Provide the actual path to the XLS file
 
-    # Step 7: Validate Data
+    # Step 8: Validate Data
     import_button = driver.find_element(By.XPATH, '//button[contains(text(),"Import")]')
     driver.execute_script("arguments[0].scrollIntoView();", import_button)
     driver.execute_script("arguments[0].click();", import_button)
@@ -53,28 +56,22 @@ try:
     driver.execute_script("arguments[0].scrollIntoView();", validate_button)
     driver.execute_script("arguments[0].click();", validate_button)
     
-    WebDriverWait(driver, 10).until(EC.alert_is_present())  # Wait until the alert is present
+    WebDriverWait(driver, 10).until(EC.alert_is_present())  
     alert = driver.switch_to.alert
-    alert.accept()  # This clicks the OK button on the alert
+    alert.accept()  
 
     # Step 8: Capture Screenshot
-    time.sleep(10)  # Add a small delay to ensure the page is fully loaded before taking a screenshot
-    # Step 9: Capture Full-Page Screenshot
-    scroll_script = "window.scrollBy(0, 5000);"  # Scroll down 500 pixels
+    time.sleep(10) 
+    scroll_script = "window.scrollBy(0, 5000);" 
     driver.execute_script(scroll_script)
     time.sleep(8) 
-    # Step 10: Capture Full-Page Screenshot after scrolling
     total_height = driver.execute_script("return document.body.parentNode.scrollHeight")
     total_width = driver.execute_script("return document.body.parentNode.scrollWidth")
-
-    driver.set_window_size(total_width, total_height)  # Adjust browser window to full page size
-
-    screenshot_path = 'full_page_screenshot_after_scroll.png'
-    driver.save_screenshot(screenshot_path)
-    
+    driver.set_window_size(total_width, total_height) 
+    screenshot_path = 'screenshot.png'
+    driver.save_screenshot(screenshot_path) 
     print(f"Full-page screenshot after scrolling saved at: {screenshot_path}")
 
 finally:
-    # Close the browser after completion
     driver.quit()
     
